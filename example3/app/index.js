c0f6e47082c27c02a47101c65547de64d6ff4edb
joshua1988 <jangkeehyo@gmail.com>
2017-10-12 01:14:14 +0900

    Updated the webpack boilerplate for tutorials

---
+++ example3/app/index.js
@@ -0,0 +1,10 @@
+// #1 - Using NPM
+// var $ = require('jquery');
+// console.log("loaded jQuery version is " + $.fn.jquery);
+
+// #2 - Using alias
+// import $ from 'Vendor/jquery-2.2.4.min.js';
+// console.log("loaded jQuery version is " + $.fn.jquery);
+
+// #3 - Using Provide Plugin
+// console.log("loaded jQuery version is " + $.fn.jquery);
